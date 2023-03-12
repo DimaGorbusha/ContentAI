270d8d8f3359473dfa8f0faea59064de8d826290
DimaGorbusha <dmitrgor1@yandex.ru>
2023-03-12 14:19:28 +0500

    feat(FormLayout): Add FormLayout

---
+++ layouts/formLayout/FormLayout.props.ts
@@ -0,0 +1,10 @@
+interface Props{
+    title1: string;
+    title2: string;
+    title3: string;
+    subtitle1: string;
+    subtitle2: string;
+    subtitle3: string;
+};
+
+export default Props;
---
+++ layouts/formLayout/FormLayout.tsx
@@ -0,0 +1,43 @@
+import Props from './FormLayout.props';
+import Button from '@/components/common/Button';
+
+const FormLayout: React.FC<Props> = ({ title1, title2, title3, subtitle1, subtitle2, subtitle3 }) => {
+	return (
+		<div>
+			<div>
+				<div className='w-[339px]'>
+					<h2 className='text-BoldBodyText_18 mb-[6px]'>
+						{title1}
+					</h2>
+					<p className='text-BodyText_14 text-greyText'>
+						{subtitle1}
+					</p>
+				</div>
+				<div className='w-[339px]'>
+					<h2 className='text-BoldBodyText_18 mb-[6px]'>
+						{title2}
+					</h2>
+					<p className='text-BodyText_14 text-greyText'>
+						{subtitle2}
+					</p>
+				</div>
+				<div className='w-[339px]'>
+					<h2 className='text-BoldBodyText_18 mb-[6px]'>
+						{title3}
+					</h2>
+					<p className='text-BodyText_14 text-greyText'>
+						{subtitle3}
+					</p>
+				</div>
+				<Button variant='filled' color='black' className='w-[360px] mt-[28px]'>
+					Сгенерировать
+				</Button>
+			</div>
+			<div>
+
+			</div>
+		</div>
+	);
+};
+
+export default FormLayout;
