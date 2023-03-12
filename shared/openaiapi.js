3ee98b6a4b118c2ccda88d4e9437ff5da2ea78da
DimaGorbusha <dmitrgor1@yandex.ru>
2023-03-12 15:43:06 +0500

    reefactor(openai): Add openai

---
+++ shared/openaiapi.js
@@ -0,0 +1,3 @@
+const OPENAI_API_KEY = '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';
+
+export default OPENAI_API_KEY;
---
+++ utils/openai.js
@@ -0,0 +1,15 @@
+import { Configuration, OpenAIApi } from 'openai';
+import OPENAI_API_KEY from '@shared/openaiapi';
+
+export const generatePrompts = (role, prompt) => {
+	const configuration = new Configuration({
+		apiKey: OPENAI_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
+
+	const completion = openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [{ role: role, content: prompt }],
+	});
+	return(completion.data.choices[0].message);
+};
